e105e985c0859e73e8b39f4722d95357c7ebc3f5
kevin <git@kevin.com>
2016-11-25 20:02:03 +0800

    最终版主界面V3

--- new-index/js/index.js
+++ new-index/js/index.js
@@ -1,27 +1,22 @@
-<<<<<<< HEAD
-//这是我在本地添加的内容
-=======
-//这是我在远程仓库添加的内容
->>>>>>> db4f35a43caaa0499558999fc6b66c1beefc78d1
-//签到和首次登陆弹框//
+//签到和首次登陆弹框
 $(function() {
     // var A=$("#new_user").val();
     // var B=$("#past").val();
-    // 定义的
-    var AA = -11;
-    var BB = -1;
+    // AA表示是否是新用户，BB表示是否第几天登陆
+    var AA = 1;
+    var BB = 3;
     if (AA == 1) {
         $(".screen3").show();
         $(".test_user_first").show();
         if (BB != -1) {
-            $(".test_user_past .past-days:nth-of-type(" + BB + ")").css("background", "blue");
+           $(".up_pd" + BB + " .up_pd_border").show();
         }
     } else if (BB != -1) {
         $(".screen3").show();
-        $(".test_user_past").show();
-        $(".test_user_past .past-days:nth-of-type(" + BB + ")").css("background", "blue");
+        $(".user_past").show();
+        $(".up_pd" + BB + " .up_pd_border").show();
     }
-    $(".test1_close").click(function() {
+    $(".user_first_close").click(function() {
         if (BB != -1) {
             $(".test_user_first").hide();
             $(".test_user_past").show();
@@ -30,19 +25,26 @@ $(function() {
             $(".test_user_past").hide();
         }
     });
+    $(".user_first_btn").click(function() {
+        if (BB != -1) {
+            $(".test_user_first").hide();
+             $(".user_past").show();
+        } else {
+            $(".screen3,.user_past").hide();
+        }
+    });
 });
-$(".test1_btn").click(function() {
-    window.open("http://www.baidu.com");
-});
-$(".test2_btn").click(function() {
+// 用户签到按钮点击签到弹出框消失，弹出充值结果并延时消失
+$(".user_past_btn").click(function() {
     $(".test_success").show();
     setTimeout(function() {
-        $(".screen3").hide();
-        $(".test_user_past").hide();
+        $(".screen3,.test_user_past").hide();
     }, 1000);
 });
 // 公告栏正方体初始角度
 var initialAngle = 0;
+// 定义游戏类型
+var gameType = 1;
 // 子导航栏以及游戏类型切换
 $('.section1').click(function() {
     $('.section2').removeClass("active2");
@@ -76,12 +78,23 @@ function test1() {
 function test2() {
     $('.games').css("-webkit-transform", "translate(-" + w_width + "px,0)");
 }
+// 领奖图标点击效果
+$(".accept_prize").touchstart(function() {
+    $(this).addClass("accept_prize_dark");
+});
+$(".accept_prize").touchend(function() {
+    $(this).removeClass("accept_prize_dark");
+});
 // 手指滑动切换模块
 $(".games").on("swipeleft", function() {
     $(".section2").click();
 });
 $(".games").on("swiperight", function() {
     $(".section1").click();
+});
+//排行和任务点击事件
+$(".rank_icon,.task_icon").click(function() {
+    alert("功能即将开启，尽请期待~");
 });
 //金币充值弹框
 $(".gold_show").click(function() {
@@ -95,9 +108,12 @@ $(".gold_show").touchend(function() {
     $(".gold_plus").removeClass("gold_plus_dark");
 });
 $(".gold_popo_up_close").click(function() {
-    $(".screen").hide();
-    $(".gold_pop_up").hide();
+    $(".screen,.gold_pop_up").hide();
 });
+$(".user_past_close").click(function() {
+    $(".screen3,.user_past").hide();
+});
+// 商城弹出框第5个充值模块动态调整充值数值
 $(".gold_lv5_minus").click(function() {
     var lvMaxGold = parseInt($(".gold_lv5_num_content span").text());
     console.log(lvMaxGold);
@@ -196,4 +212,20 @@ $(".rsp_confirm_btn").touchstart(function() {
 });
 $(".rsp_confirm_btn").touchend(function() {
     $(this).removeClass("rsp_confirm_btn_dark");
+});
+// 点击游戏图标切换游戏
+$(".lol_icon_content").click(function() {
+    if (gameType == 1) {
+        $(".lol_content").css("-webkit-animation", "head_icon1 1s ease");
+        gameType = 2;
+        setTimeout(function() {
+            $(".lol_content").css("background-position", "-37px 0");
+        }, 500);
+    } else {
+        $(".lol_content").css("-webkit-animation", "head_icon2 1s ease");
+        gameType = 1;
+        setTimeout(function() {
+            $(".lol_content").css("background-position", "0 0");
+        }, 500);
+    }
 });
